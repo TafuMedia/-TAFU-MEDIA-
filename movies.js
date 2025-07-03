cd62d85270ff78c6cfab4663061c63fc8059df65
TafuMedia <ismatjahanakhi63@gmail.com>
2025-07-04 05:41:40 +0600

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,9 @@
+const movies = [
+  {
+    title: "Big Hero 6",
+    category: "Animated Action • Family",
+    description: "Young robotics prodigy Hiro teams up with a robot and friends to save the city in this animated action adventure.",
+    poster: "https://drive.google.com/uc?export=view&id=1PJgJGPdQg9W1fyqhVozVCqkLv402j4VY",
+    videoId: "1s8x_RIV_cM0pIfzc22t4k-ybKLYiHhN3"
+  }
+];
