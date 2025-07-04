d149a5e6e25e44c0d5c419ba81129037c9a5c68f
TafuMedia <ismatjahanakhi63@gmail.com>
2025-07-04 06:31:19 +0600

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,10 @@
+const movies = [
+  {
+    title: "96",
+    category: "Romantic Drama",
+    description: "A touching love story of two high school sweethearts meeting after years.",
+    poster: "https://i.ibb.co/Y7KbsBvh/96-movie-poster.jpg",
+    videoId: "63F9Pv4k4wk",
+    sourceType: "youtube"
+  }
+];
